f034dad8323a4eb4f47781a5652eadc6d60073e6
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-02-23 10:13:05 +0530

    Sample added
    Sample added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Syncfusion.Data;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+using System.Windows;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }        
+    }
+}
+         
+   
+
